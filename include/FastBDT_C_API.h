0b64106fae7844be7a49458c8c272cd34bb93182
Giacomo De Pietro <giacomo.pietro@kit.edu>
2026-06-17 23:33:15 +0200

    Run astyle and flake8 over the entire codebase

--- include/FastBDT_C_API.h
+++ include/FastBDT_C_API.h
@@ -8,67 +8,67 @@
 
 extern "C" {
 
-    void PrintVersion();
+  void PrintVersion();
 
-    bool IsWeightFloat();
+  bool IsWeightFloat();
 
-    struct Expertise {
-      FastBDT::Classifier classifier;
-    };
-      
-    void* Create();
+  struct Expertise {
+    FastBDT::Classifier classifier;
+  };
 
-    void SetBinning(void *ptr, unsigned int* binning, unsigned int size);
-    void SetPurityTransformation(void *ptr, bool* purityTransformation, unsigned int size);
-    
-    void SetNTrees(void *ptr, unsigned int nTrees);
-    unsigned int GetNTrees(void *ptr);
-    
-    void SetDepth(void *ptr, unsigned int depth);
-    unsigned int GetDepth(void *ptr);
-    
-    void SetNumberOfFlatnessFeatures(void *ptr, unsigned int numberOfFlatnessFeatures);
-    unsigned int GetNumberOfFlatnessFeatures(void *ptr);
-    
-    void SetSubsample(void *ptr, double subsample);
-    double GetSubsample(void *ptr);
-    
-    void SetShrinkage(void *ptr, double shrinkage);
-    double GetShrinkage(void *ptr);
-    
-    void SetFlatnessLoss(void *ptr, double flatnessLoss);
-    double GetFlatnessLoss(void *ptr);
+  void* Create();
 
-    void SetTransform2Probability(void *ptr, bool transform2probability);
-    bool GetTransform2Probability(void *ptr);
-    
-    void SetSPlot(void *ptr, bool sPlot);
-    bool GetSPlot(void *ptr);
-    
-    void Delete(void *ptr);
-    
-    void Fit(void *ptr, float *data_ptr, FastBDT::Weight *weight_ptr, bool *target_ptr, unsigned int nEvents, unsigned int nFeatures);
+  void SetBinning(void* ptr, unsigned int* binning, unsigned int size);
+  void SetPurityTransformation(void* ptr, bool* purityTransformation, unsigned int size);
 
-    void Load(void* ptr, char *weightfile);
+  void SetNTrees(void* ptr, unsigned int nTrees);
+  unsigned int GetNTrees(void* ptr);
 
-    float Predict(void *ptr, float *array);
+  void SetDepth(void* ptr, unsigned int depth);
+  unsigned int GetDepth(void* ptr);
 
-    void PredictArray(void *ptr, float *array, float *result, unsigned int nEvents);
+  void SetNumberOfFlatnessFeatures(void* ptr, unsigned int numberOfFlatnessFeatures);
+  unsigned int GetNumberOfFlatnessFeatures(void* ptr);
 
-    void Save(void* ptr, char *weightfile);
-    
-    struct VariableRanking {
-        std::map<unsigned int, double> ranking;
-    }; 
+  void SetSubsample(void* ptr, double subsample);
+  double GetSubsample(void* ptr);
 
-    void* GetVariableRanking(void* ptr);
-    
-    void* GetIndividualVariableRanking(void* ptr, float *array);
-    
-    unsigned int ExtractNumberOfVariablesFromVariableRanking(void* ptr);
-    
-    double ExtractImportanceOfVariableFromVariableRanking(void* ptr, unsigned int iFeature);
-    
-    void DeleteVariableRanking(void* ptr);
+  void SetShrinkage(void* ptr, double shrinkage);
+  double GetShrinkage(void* ptr);
+
+  void SetFlatnessLoss(void* ptr, double flatnessLoss);
+  double GetFlatnessLoss(void* ptr);
+
+  void SetTransform2Probability(void* ptr, bool transform2probability);
+  bool GetTransform2Probability(void* ptr);
+
+  void SetSPlot(void* ptr, bool sPlot);
+  bool GetSPlot(void* ptr);
+
+  void Delete(void* ptr);
+
+  void Fit(void* ptr, float* data_ptr, FastBDT::Weight* weight_ptr, bool* target_ptr, unsigned int nEvents, unsigned int nFeatures);
+
+  void Load(void* ptr, char* weightfile);
+
+  float Predict(void* ptr, float* array);
+
+  void PredictArray(void* ptr, float* array, float* result, unsigned int nEvents);
+
+  void Save(void* ptr, char* weightfile);
+
+  struct VariableRanking {
+    std::map<unsigned int, double> ranking;
+  };
+
+  void* GetVariableRanking(void* ptr);
+
+  void* GetIndividualVariableRanking(void* ptr, float* array);
+
+  unsigned int ExtractNumberOfVariablesFromVariableRanking(void* ptr);
+
+  double ExtractImportanceOfVariableFromVariableRanking(void* ptr, unsigned int iFeature);
+
+  void DeleteVariableRanking(void* ptr);
 
 }
